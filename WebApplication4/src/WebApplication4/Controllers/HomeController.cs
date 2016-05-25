28b3c076b02060de4677e2799fa63ad06b117eea
dtinsley333 <denise.tinsley@tekcongrp.com>
2016-05-24 20:05:43 -0500

    initial commit of chinook api code

---
+++ WebApplication4/src/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Linq;
+using System.Web.Http;
+using Microsoft.AspNet.Mvc;
+
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+  
+    [Produces("application/json")]
+    public class HomeController : ApiController
+    {
+        private ApplicationDbContext dbContext { get; set; }
+        public HomeController(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+                var products = dbContext.Album.ToList();
+
+                return Json(products);
+            
+        }
+       
+    }
+}
---
+++ WebApplication4/src/WebApplication4/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+    }
+}
